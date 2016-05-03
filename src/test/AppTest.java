45a1d32abde1fe2955e243ec5298248a13bd2ca1
Anthony Gold and DJ Roychoudhury <dj@>
2016-05-03 08:58:31 -0700

    adds front-end for database

--- src/test/AppTest.java
+++ src/test/AppTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.*;
 import static org.junit.Assert.*;
+import org.sql2o.*;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
@@ -20,11 +21,26 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  @Test
- public void rootTest() {
-   goTo("http://localhost:4567/");
-   assertThat(pageSource()).contains("Todo list!");
-   assertThat(pageSource()).contains("View Category List");
-   assertThat(pageSource()).contains("Add a New Category");
- }
-} 
+  @Before
+  public void setUp() {
+    DB.sql2o = new sql2o("jdbc:postgresql://localhost:5432/to_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.spq2o.open()) {
+      String deleteTasksQuery = "DELETE FROM tasks *;";
+      String deleteCategoriesQuery = "DELETE FROM categories *;";
+      con.createQuery(deleteTasksQuery).executeUpdate();
+      con.createQuery(deleteCategoriesQuery).executeUpdate();
+    }
+  }
+
+ //  @Test
+ // public void rootTest() {
+ //   goTo("http://localhost:4567/");
+ //   assertThat(pageSource()).contains("Todo list!");
+ //   assertThat(pageSource()).contains("View Category List");
+ //   assertThat(pageSource()).contains("Add a New Category");
+ // }
+}
